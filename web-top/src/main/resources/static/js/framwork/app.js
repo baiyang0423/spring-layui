04d0d8093451fa8dd2a882bdb0384a365d4b2bea
by_csd_hlj <by_csd_hlj@si-tech.com.cn>
2019-10-17 13:10:06 +0800

    优化首页

--- web-top/src/main/resources/static/js/framwork/app.js
+++ web-top/src/main/resources/static/js/framwork/app.js
@@ -1,15 +1,19 @@
-layui.use(['table',"layer","jquery","form"], function(){
+layui.use(['table',"layer","jquery","form",'element','carousel'], function(){
 
-    var $ = layui.jquery ;
-    var menus = "";
+    var $ = layui.jquery ,table = layui.table
+        ,element = layui.element,layer = layui.layer
+        ,carousel = layui.carousel;
 
+    /*******************************************************/
+    /*                                                     */
+    /*              初始化快捷菜单                         */
+    /*                                                     */
+    /*******************************************************/
     $.get('/web-top/sys/function/index/data/all', "", function(res) {
         //初始化快捷菜单
         var _quickMenu = QuickMenuData("100001" , res.data);
         $("#layui-quick-module").html(_quickMenu);
     }, 'json');
-
-
     /**
      * 初始化快捷菜单
      * @param pid
@@ -36,10 +40,94 @@ layui.use(['table',"layer","jquery","form"], function(){
 
             }
         }
-
         return str;
     }
 
+    /*******************************************************/
+    /*                                                     */
+    /*              初始化任务模块                         */
+    /*                                                     */
+    /*******************************************************/
 
+    //监听Tab切换
+    element.on('tab(demo)', function(data){
+        alert("111");
+        layer.tips('切换了 '+ data.index +'：'+ this.innerHTML, this, {
+            tips: 1
+        });
+    });
+
+
+    //初始化全部表格
+    table.render({
+        elem: '#LAY-app-message-all'
+        ,url:'/web-top/sys/function/select/page'
+        ,title: '用户数据表'
+        ,page: {
+            layout: [ 'prev', 'page', 'next','limit', 'count', 'skip'] //自定义分页布局
+            ,groups: 5 //只显示 1 个连续页码
+            ,limit:5
+            ,limits: [5, 10, 15,20]
+            ,first: true //不显示首页
+            ,last: true //不显示尾页
+            ,theme: '#FF5722'
+        }
+        ,cols: [[
+            {field:'title',  title: '标题'}
+            ,{field:'code',  title: '代码'}
+            ,{field:'url', title: '路径'}
+        ]]
+    });
+    //初始化代办表格
+    table.render({
+        elem: '#LAY-app-message-none'
+        ,url:'/web-top/sys/user/select/page'
+        ,title: '用户数据表'
+        ,page: {
+            layout: [ 'prev', 'page', 'next','limit', 'count', 'skip'] //自定义分页布局
+            ,groups: 5 //只显示 1 个连续页码
+            ,limit:5
+            ,limits: [5, 10, 15,20]
+            ,first: true //不显示首页
+            ,last: true //不显示尾页
+            ,theme: '#FF5722'
+        }
+        ,cols: [[
+            {field:'name',  title: '标题'}
+            ,{field:'password',  title: '代码'}
+            ,{field:'account', title: '路径'}
+        ]]
+    });
+    //初始化已办表格
+    table.render({
+        elem: '#LAY-app-message-done'
+        ,url:'/web-top/sys/function/select/page'
+        ,title: '用户数据表'
+        ,page: {
+            layout: [ 'prev', 'page', 'next','limit', 'count', 'skip'] //自定义分页布局
+            ,groups: 5 //只显示 1 个连续页码
+            ,limit:5
+            ,limits: [5, 10, 15,20]
+            ,first: true //不显示首页
+            ,last: true //不显示尾页
+            ,theme: '#FF5722'
+        }
+        ,cols: [[
+            {field:'title',  title: '标题'}
+            ,{field:'code',  title: '代码'}
+            ,{field:'url', title: '路径'}
+        ]]
+    });
+
+    /*******************************************************/
+    /*                                                     */
+    /*              初始化轮播模块                         */
+    /*                                                     */
+    /*******************************************************/
+    carousel.render({
+        elem: '#lay-news'
+        ,width: '100%'
+        ,interval: 2000
+    });
 
 })
